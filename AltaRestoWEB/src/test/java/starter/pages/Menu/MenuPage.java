b64c6fecc50f95eebe0e0b16e67d713b24700943
Dikaafs <redmarl377@gmail.com>
2023-12-21 12:42:15 +0700

    update menu page

--- AltaRestoWEB/src/test/java/starter/pages/Menu/MenuPage.java
+++ AltaRestoWEB/src/test/java/starter/pages/Menu/MenuPage.java
@@ -2,6 +2,9 @@ package starter.pages.Menu;
 
 import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class MenuPage extends PageObject {
     private By TambahkanMenu(){
@@ -31,7 +34,16 @@ public class MenuPage extends PageObject {
     private By notifyStatusChanged(){
         return By.xpath("/html/body/div/div[1]/div/div/div[2]");
     }
+    private By inputNameMenu(){
+        return By.xpath("/html/body/div/div[2]/div[2]/div[2]/div/div[1]/div[1]/input");
+    }
 
+    private By MenuDisplayed(){
+        return By.xpath("/html/body/div/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr/td[1]/img");
+    }
+    private By DeletedMenu(){
+        return By.xpath("/html/body/div/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[1]/td[2]");
+    }
     public void ClickTambahkanMenu(){
         $(TambahkanMenu()).click();
     }
@@ -61,5 +73,27 @@ public class MenuPage extends PageObject {
     }
     public void NotifyStatusChanged(){
         $(notifyStatusChanged()).isDisplayed();
+    }
+    public void InputNameMenu(String menu){
+        $(inputNameMenu()).type(menu);
+    }
+    public boolean validateMenuDisplayed(){
+        return $(MenuDisplayed()).isDisplayed();
+    }
+    public boolean validateDeletedMenuNotDisplayed() {
+        try {
+            WebElement element = getDriver().findElement(By.xpath("/html/body/div/div[2]/div[2]/div[2]/div/div[2]/table/tbody/tr[1]/td[2]"));
+
+            // Jika elemen ditemukan, periksa apakah terlihat
+            if (element.isDisplayed()) {
+                System.out.println("Elemen ditemukan dan terlihat.");
+            } else {
+                System.out.println("Elemen ditemukan tetapi tidak terlihat.");
+            }
+        } catch (NoSuchElementException e) {
+            // Jika NoSuchElementException terjadi, elemen tidak ada
+            System.out.println("Elemen tidak ditemukan.");
+        }
+        return true;
     }
 }
